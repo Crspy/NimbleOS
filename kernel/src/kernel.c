c0e48b9de9720e54ddc0f73691414f5c437144a3
Crspy <crspy2020@gmail.com>
2023-03-18 21:59:43 +0200

    added available memory report on welcome screen

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -10,8 +10,7 @@
 #include <kernel/timer.h>
 #include <kernel/cpu.h>
 
-
-void kernel_main(multiboot_info_t *, uint32_t magic)
+void kernel_main(multiboot_info_t *mbi, uint32_t magic)
 {
 	term_init();
 	gdt_init();
@@ -28,11 +27,44 @@ void kernel_main(multiboot_info_t *, uint32_t magic)
 
 	printf("Welcome to \x1B[0;33mNimble OS\x1B[37m v1.0 !\n\n");
 
+
+	if (mbi->flags & MULTIBOOT_INFO_MEM_MAP)
+	{
+		// I didn't even know these worked in protected mode
+		// Looking at the assembly, I think they just take up more space,
+		// but they're still processed by 32-bits registers
+		uint64_t available = 0;
+		uint64_t unavailable = 0;
+		multiboot_memory_map_t *mmap = (multiboot_memory_map_t *)mbi->mmap_addr;
+
+		for (mmap = (multiboot_memory_map_t *)mbi->mmap_addr;
+			 (unsigned long)mmap < mbi->mmap_addr + mbi->mmap_length;
+			 mmap = (multiboot_memory_map_t *)((unsigned long)mmap + mmap->size + sizeof(mmap->size)))
+		{
+			if (mmap->type == 1)
+			{
+				available += mmap->len;
+			}
+			else
+			{
+				unavailable += mmap->len;
+			}
+		}
+
+		printf("Total available memory: \x1B[32m%dMiB\x1B[37m\n", available >> 20);
+		printf("Total unavailable memory: \x1B[32m%dKiB\x1B[37m\n", unavailable >> 10);
+	}
+	else
+	{
+		printf("ERROR: No memory map given, catching fire...\n");
+		abort();
+	}
+
 	uint32_t time_start = timer_get_time();
-	while (1) 
+	while (1)
 	{
 		uint32_t time_now = timer_get_time();
-		if (time_now != time_start) 
+		if (time_now != time_start)
 		{
 			time_start = time_now;
 			printf("time: %d\n", time_now); // will print every 1s
